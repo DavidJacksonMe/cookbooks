fba941cf905152be4996eae923b3977b59e18dea
David Jackson <david.jackson@redlea.com>
2021-05-25 12:27:47 +0100

    added db creds to logger

--- cookbook1/recipes/default.rb
+++ cookbook1/recipes/default.rb
@@ -5,6 +5,7 @@
 # Copyright:: 2021, The Authors, All Rights Reserved.
 #app = search(:aws_opsworks_app).first
 log 'message' do
-  message "A message add to the log - #{node['platform']}"
+  message "Database username - #{node['deploy']['app_name']['database']['username']}"
+  message "Database password - #{node['deploy']['app_name']['database']['password']}"
   level :info
 end
