3551d8acd0895153a76f2b502d9f63efcb5abbd2
David Jackson <david.jackson@redlea.com>
2021-05-25 15:56:35 +0100

    more changes

--- cookbook1/recipes/default.rb
+++ cookbook1/recipes/default.rb
@@ -3,8 +3,27 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
-#app = search(:aws_opsworks_app).first
-log 'message' do
-  message "Database username - #{node.default['deploy']['app_name']['database']['username']}, password - #{node.default['deploy']['app_name']['database']['password']}"
-  level :info
+#db = search(:node, "database").first
+#db = node[:aws_opsworks_app][:deploy][:app_name][:database]
+#log 'message' do
+  #app = search(:node, "aws_opsworks_app").first
+  #app = search("aws_opsworks_app")
+  #db = node[:aws_opsworks_app][:deploy][:app_name][:database]
+  #message "Database username - #{db[:username]}, password - #{db[:password]}"
+  #message "Database username - #{db}"
+  #message "hello"
+  #message "AppShortname - '#{app['shortname']}', Database username - #{app['deploy']['app_name']['database']['username']}, password - #{app['deploy']['app_name']['database']['password']}"
+  #message "AppShortname - #{app['shortname']}"
+  #message "AppShortname - #{app[:shortname]}"
+  #message "AppShortname - #{app[:shortname]}, Database username - #{app[:deploy][:app_name][:database][:username]}, password - #{app[:deploy][:app_name][:database][:password]}"
+  #level :info
+#end
+
+node[:aws_opsworks_app][:deploy].each do |app_name, app_name_data|
+  log "database: #{app_name_data}"
+  log "username: #{app_name_data['database']['username']}"
+  log "password: #{app_name_data['database']['password']}"
+  #layerdata['instances'].each do |instance, instancedata|
+  #  log "Public IP: #{instancedata['ip']}"
+  #end
 end
