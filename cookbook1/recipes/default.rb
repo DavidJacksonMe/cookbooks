8eb294dab136b976a7fb22b4f7e2c9b0e7dcc2eb
David Jackson <david.jackson@redlea.com>
2021-05-25 12:59:55 +0100

    changes to recipe

--- cookbook1/recipes/default.rb
+++ cookbook1/recipes/default.rb
@@ -5,7 +5,6 @@
 # Copyright:: 2021, The Authors, All Rights Reserved.
 #app = search(:aws_opsworks_app).first
 log 'message' do
-  message "Database username - #{node['deploy']['app_name']['database']['username']}"
-  message "Database password - #{node['deploy']['app_name']['database']['password']}"
+  message "Database username - #{node.default['deploy']['app_name']['database']['username']}, password - #{node.default['deploy']['app_name']['database']['password']}"
   level :info
 end
