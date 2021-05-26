5291dc861440ae3c3e296e908172563f14d51fe3
David Jackson <david.jackson@redlea.com>
2021-05-26 10:05:23 +0100

    changes

--- cookbook1/recipes/default.rb
+++ cookbook1/recipes/default.rb
@@ -5,16 +5,18 @@
 # Copyright:: 2021, The Authors, All Rights Reserved.
 #db = search(:node, "database").first
 #db = node[:aws_opsworks_app][:deploy][:app_name][:database]
+#db = node[:aws_opsworks_app][:deploy][:app_name][:database]
+app = search("aws_opsworks_app").first
 log 'message' do
   #app = search(:node, "aws_opsworks_app").first
   #app = search("aws_opsworks_app")
   #db = node[:aws_opsworks_app][:deploy][:app_name][:database]
   #message "Database username - #{db[:username]}, password - #{db[:password]}"
   #message "Database username - #{db}"
-  message "aws_opsworks_app=#{node[:aws_opsworks_app]}"
-  message "aws_opsworks_rds_db_instance=#{node[:aws_opsworks_rds_db_instance]}"
+  #message "aws_opsworks_app=#{node[:aws_opsworks_app]}"
+  #message "aws_opsworks_rds_db_instance=#{node[:aws_opsworks_rds_db_instance]}"
   #message "AppShortname - '#{app['shortname']}', Database username - #{app['deploy']['app_name']['database']['username']}, password - #{app['deploy']['app_name']['database']['password']}"
-  #message "AppShortname - #{app['shortname']}"
+  message "AppShortname - #{app['shortname']}"
   #message "AppShortname - #{app[:shortname]}"
   #message "AppShortname - #{app[:shortname]}, Database username - #{app[:deploy][:app_name][:database][:username]}, password - #{app[:deploy][:app_name][:database][:password]}"
   level :info
